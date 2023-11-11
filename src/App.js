ba3c349de2dfa7bfedd0cd1b091ea346149516f9
devkyoung2 <devkyoung2@gmail.com>
2023-11-12 05:13:40 +0900

    feat : 앱 초기 세팅
    입력 받기 전 필요한 객체 세팅

--- src/App.js
+++ src/App.js
@@ -1,4 +1,16 @@
+import User from './domains/User.js';
+import Menu from './domains/Menu.js';
+import InputView from './views/InputView.js';
+import OutputView from './views/OutputView.js';
+
 class App {
+  #user;
+  #menu;
+
+  constructor() {
+    this.#user = new User();
+    this.#menu = new Menu();
+  }
   async run() {}
 }
 
