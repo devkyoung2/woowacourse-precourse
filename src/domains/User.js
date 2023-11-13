d31dcd3e3dfefa08773a8e4100d2c162d24dc052
devkyoung2 <devkyoung2@gmail.com>
2023-11-14 03:47:05 +0900

    feat : 주문 메뉴 파싱
    주문 메뉴 문자열을 파싱해 메뉴 형식이 예시와 다른 경우 에러를 발생시킨다.

--- src/domains/User.js
+++ src/domains/User.js
@@ -1,11 +1,20 @@
 import { isNatural } from '../utils/validate.js';
 
-class User {
+export default class User {
   #date;
+  #orderItem;
 
   setDate(date) {
     this.#validationDate(date);
     this.#date = date;
+  }
+
+  // 함수명 수정
+  setOrderMenu(order) {
+    const parsedOrder = parsingOrder(order);
+    this.#validationOrder(parsedOrder);
+    // orderMenu 이상 없을 시, 주문 인스턴스 생성
+    // 생성된 객체 내부에서 주문 유효성 검증
   }
 
   #validationDate(date) {
@@ -19,6 +28,26 @@ class User {
 
     return dateNumber;
   }
+
+  #validationOrder(parsedOrder) {
+    // 유효성 확인
+  }
 }
 
-export default User;
+function parsingOrder(orders) {
+  const parsedOrders = {};
+
+  for (const order of orders.split(',')) {
+    const parsedOrder = order.split('-');
+
+    if (parsedOrder.length !== 2) {
+      throw new Error('[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.');
+    }
+
+    const [item, count] = parsedOrder;
+
+    parsedOrders[item] = parseInt(count) || 0;
+  }
+
+  return parsedOrders;
+}
