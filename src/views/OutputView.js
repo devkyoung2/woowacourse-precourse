933f25cd54ab25061b2aec4cf71f67baf2dcbb84
devkyoung2 <devkyoung2@gmail.com>
2023-11-14 00:15:41 +0900

    docs : 템플릿 변경
    InputView와 OutputView의 export하는 부분 변경/경로이동

---
+++ src/views/OutputView.js
@@ -0,0 +1,10 @@
+import { Console } from '@woowacourse/mission-utils';
+
+const OutputView = {
+  printMenu() {
+    Console.print('<주문 메뉴>');
+    // ...
+  },
+  // ...
+};
+export default OutputView;
