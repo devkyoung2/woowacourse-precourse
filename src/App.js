b110cc041cf9249c044991b06d7ed0043bce065e
devkyoung2 <devkyoung2@gmail.com>
2023-11-14 01:31:13 +0900

    feat : 식당 예상 방문 날짜 입력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import InputView from './views/InputView.js';
+import OutputView from './views/OutputView.js';
+
 class App {
-  async run() {}
+  async run() {
+    OutputView.printWecome();
+    await this.inputDate();
+  }
+
+  async inputDate() {
+    try {
+      const date = await InputView.readDate();
+      console.log(date);
+    } catch (error) {
+      // 추후 상수화
+      Console.print(error.message);
+      return this.inputDate();
+    }
+  }
 }
 
 export default App;
--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -1,6 +1,10 @@
 import { Console } from '@woowacourse/mission-utils';
 
 const OutputView = {
+  printWecome() {
+    Console.print('안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.\n');
+  },
+
   printMenu() {
     Console.print('<주문 메뉴>');
     // ...
