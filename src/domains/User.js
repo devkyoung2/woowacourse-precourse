d6bec2d8a81a6757bef3dfd9605b90077d04a92b
devkyoung2 <devkyoung2@gmail.com>
2023-11-14 06:27:57 +0900

    feat : 중복된 메뉴 입력시 에러 처리

--- src/domains/User.js
+++ src/domains/User.js
@@ -11,8 +11,7 @@ export default class User {
 
   // 함수명 수정
   setOrderMenu(order) {
-    const parsedOrder = parsingOrder(order);
-    this.#validationOrder(parsedOrder);
+    this.#validationOrder(order);
     // orderMenu 이상 없을 시, 주문 인스턴스 생성
     // 생성된 객체 내부에서 주문 유효성 검증
   }
@@ -29,13 +28,22 @@ export default class User {
     return dateNumber;
   }
 
-  #validationOrder(parsedOrder) {
-    // 유효성 확인
+  #validationOrder(order) {
+    const parsedOrder = parsingOrder(order);
+    isDuplicatedOrder(parsedOrder);
+    hasOrderInMenu(parsedOrder);
+    // 메뉴판에 없는 메뉴가 입력되면 안됨
+    // 메뉴의 개수는 1이상의 숫자
   }
 }
 
+function hasOrderInMenu(parsedOrder) {
+  parsedOrder.some((item) => console.log(item));
+  // console.log(parsedOrder);
+}
+
 function parsingOrder(orders) {
-  const parsedOrders = {};
+  const parsedOrders = [];
 
   for (const order of orders.split(',')) {
     const parsedOrder = order.split('-');
@@ -43,11 +51,22 @@ function parsingOrder(orders) {
     if (parsedOrder.length !== 2) {
       throw new Error('[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.');
     }
-
     const [item, count] = parsedOrder;
-
-    parsedOrders[item] = parseInt(count) || 0;
+    parsedOrders.push({ [item]: count });
   }
 
   return parsedOrders;
+}
+
+function isDuplicatedOrder(orderArr) {
+  const orderObj = {};
+
+  orderArr.forEach((order) => {
+    const [key, value] = Object.entries(order)[0];
+    orderObj[key] = Number(value);
+  });
+
+  if (orderArr.length !== Object.keys(orderObj).length) {
+    throw new Error('[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.');
+  }
 }
