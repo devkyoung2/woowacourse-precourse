42722f70a17c12028f865f4f1c3e44a5d2e99b26
devkyoung2 <devkyoung2@gmail.com>
2023-11-14 13:59:06 +0900

    feat : 주문 메뉴가 20개 이하인지 확인하는 로직 구현

--- src/domains/Order.js
+++ src/domains/Order.js
@@ -9,9 +9,17 @@ class Order {
     if (Menu.isOnlyDrink(orders))
       throw new Error('[ERROR] 음료만 주문할 수 없습니다.');
 
+    if (countOrderItems(orders) > 20) {
+      throw new Error('[ERROR] 메뉴는 20개까지 주문할 수 있습니다.');
+    }
+
     // - [] 총 주문 금액이 10000원 이상이어야 한다.
-    // - [] 메뉴는 20개까지 주문할 수 있다.
   }
+}
+
+function countOrderItems(orders) {
+  const items = orders.map((item) => item.count);
+  return items.reduce((acc, cur) => acc + cur);
 }
 
 export default Order;
