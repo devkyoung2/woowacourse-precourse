c6f7628f74458945e987836fa02b03b064219bce
chestnut1044 <wbk1620@gmail.com>
2023-11-28 17:02:21 +0900

    feat : 사용자에게 숫자 입력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,16 +1,50 @@
 import InputView from './views/InputView.js';
 import OutputView from './views/OutputView.js';
+import { Random } from '@woowacourse/mission-utils';
 import Computer from './models/Computer.js';
+import Gong from './models/Gong.js';
 
 class App {
-  #computer = new Computer();
+  #computer;
   #user;
 
   async play() {
     OutputView.printWelcomeGame();
-    this.#computer.generatorGong();
+    this.gameStart();
+  }
+
+  async gameStart() {
+    this.#computer = this.generateRandom(3);
+
+    try {
+      const inputUserGuess = await InputView.readGong();
+      this.#user = [...inputUserGuess];
+    } catch (err) {
+      console.log(err.message);
+    }
+    console.log(this.#computer, this.#user);
+    this.judgeStrikeAndBall(this.#user, this.#computer);
+  }
+
+  generateRandom(digit) {
+    let pickedNumbers = '';
+
+    while (pickedNumbers.length < digit) {
+      const pickedNumber = Random.pickNumberInRange(1, 9);
+      if (!pickedNumbers.includes(pickedNumber)) {
+        pickedNumbers += pickedNumber;
+      }
+    }
+
+    return [...pickedNumbers];
+  }
+
+  judgeStrikeAndBall(user, computer) {
+    let strike = 0;
+    let ball = 0;
   }
 }
+
 const app = new App();
 app.play();
 
--- src/views/InputView.js
+++ src/views/InputView.js
@@ -1,11 +1,21 @@
 import { Console } from '@woowacourse/mission-utils';
 
 const InputView = {
-  async readDate() {
-    const input = await Console.readLineAsync();
-    // ...
+  async readGong() {
+    const input = await Console.readLineAsync('숫자를 입력해주세요 : ');
+    this.isValidate(input);
+
+    return input;
   },
-  // ...
+
+  isValidate(input) {
+    if (input.length === 0) {
+      throw new Error('[Error] : 올바른 값이 아닙니다.');
+    }
+    if (input.trim().length === 0) {
+      throw new Error('[Error] : 올바른 값이 아닙니다.');
+    }
+  },
 };
 
 export default InputView;
