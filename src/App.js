eef65b00c9c32b3faebd684397cb061393e2cf37
chestnut1044 <wbk1620@gmail.com>
2023-11-30 17:29:40 +0900

    feat : 자동차 경주 기능 구현
    - 매 경기 랜덤수 생성
    - 생성한 수가 4 이상이면 전진

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import InputView from './views/InputView.js';
 import OutputView from './views/OutputView.js';
+import { Random } from '@woowacourse/mission-utils';
 // import Race from './models/Race.js';
 
 class App {
@@ -11,31 +12,48 @@ class App {
     // 입력부분 분리하기
     try {
       const cars = await InputView.readCars();
-      this.#validateCars(cars);
-      this.#cars = cars;
+      const validateCars = this.#validateCars(cars);
+      this.#cars = validateCars;
 
       const attempts = await InputView.readAttempts();
-      this.#validateAttempts(attempts);
-      this.#attempts = attempts;
+      const validatedAttempts = this.#validateAttempts(attempts);
+      this.#attempts = validatedAttempts;
     } catch (err) {
       console.log(err.message);
       return;
     }
-    this.raceStart();
+    this.raceStart(this.#cars, this.#attempts);
   }
-  raceStart() {}
+
+  raceStart(cars, attempts) {
+    for (let i = 0; i <= attempts; i++) {
+      this.round(cars);
+    }
+    console.log(this.#cars);
+  }
+
+  round(cars) {
+    for (const car of cars) {
+      if (Random.pickNumberInRange(0, 9) > 4) {
+        car.position += 1;
+      }
+    }
+  }
 
   #validateCars(cars) {
     const splitedCars = cars.split(',');
 
+    const validatedCars = [];
     for (const car of splitedCars) {
       const trimmedCarName = car.trim();
 
       if (trimmedCarName.length > 5) {
         throw new Error('5글자 이상의 이름');
       }
+      validatedCars.push({ name: trimmedCarName, position: 0 });
     }
-    return splitedCars;
+
+    return validatedCars;
   }
 
   #validateAttempts(attempts) {
@@ -49,6 +67,8 @@ class App {
     if (attemptsNumber < 1) {
       throw new Error('1 이하의 값');
     }
+
+    return attemptsNumber;
   }
 }
 
