4982af8413e94a159c2deee27cb0614f68ae7d67
chestnut1044 <wbk1620@gmail.com>
2023-11-30 17:09:44 +0900

    feat : 자동차 이름 유효성 검증

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import InputView from './views/InputView.js';
 import OutputView from './views/OutputView.js';
+// import Race from './models/Race.js';
 
 class App {
-  #race;
+  // #race = new Race();
+  #cars;
+  #attempts;
 
   async play() {
+    // 입력부분 분리하기
     try {
-      const { cars, attempts } = this.InputUser();
-      this.#race = new Race(cars, attempts);
+      const cars = await InputView.readCars();
+      this.#validateCars(cars);
+      this.#cars = cars;
+
+      const attempts = await InputView.readAttempts();
+      this.#validateAttempts(attempts);
+      this.#attempts = attempts;
     } catch (err) {
       console.log(err.message);
+      return;
     }
+    this.raceStart();
+  }
+  raceStart() {}
 
-    this.#race.start();
+  #validateCars(cars) {
+    const splitedCars = cars.split(',');
+
+    for (const car of splitedCars) {
+      const trimmedCarName = car.trim();
+
+      if (trimmedCarName.length > 5) {
+        throw new Error('5글자 이상의 이름');
+      }
+    }
+    return splitedCars;
   }
 
-  async InputUser() {
-    const cars = await InputView.readCars();
-    const attempts = await InputView.readAttempts();
-
-    return { cars, attempts };
-  }
+  #validateAttempts(attempts) {}
 }
 
 const app = new App();
--- src/views/InputView.js
+++ src/views/InputView.js
@@ -5,14 +5,16 @@ const InputView = {
     const input = await Console.readLineAsync(
       '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
     );
-
     validateEmptyInput(input);
+
+    return input;
   },
 
   async readAttempts() {
     const input = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
-
     validateEmptyInput(input);
+
+    return input;
   },
 };
 
