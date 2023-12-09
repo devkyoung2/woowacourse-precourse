8dcc982aaf31a1c7e4a763890cd343a327ca69ea
chestnut1044 <wbk1620@gmail.com>
2023-12-09 21:06:12 +0900

    feat : 게임 시작 안내 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import BridgeGameController from './controllers/BridgeGameController.js';
+
 class App {
-  play() {}
+  #bridgeGameController;
+
+  // Todo : 의존성 주입 이게 맞는지 확인하기..
+  play(bridgeGameController = new BridgeGameController()) {
+    this.#bridgeGameController = bridgeGameController;
+    this.#bridgeGameController.run();
+  }
 }
 
 const app = new App();
---
+++ src/controllers/BridgeGameController.js
@@ -0,0 +1,9 @@
+import OutputView from '../views/OutputView.js';
+
+class BridgeGameController {
+  run() {
+    OutputView.printWelcomeMessage();
+  }
+}
+
+export default BridgeGameController;
